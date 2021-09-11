1af5f77137d2f7f3f7a9ba7377daccda79e8d21e
alice <3859397+fp-alice@users.noreply.github.com>
2021-09-11 15:59:38 -0400

    add functions for bit/byte conversion

--- lispchat.lisp
+++ lispchat.lisp
@@ -6,8 +6,10 @@
 
 ;; Recursive function to collect the bits of the integer one-by-one
 (defun recurse-collect-bits (integer carry)
-    (if (equalp integer 0)
-      carry                               ;; Return our collected list if we've read all bits
+    (if (zerop integer)
+      (if carry
+          carry                           ;; Return carry if int has been consumed and carry is not nil
+          '(0))                           ;; Return 0 if empty carry
       (recurse-collect-bits               ;; Otherwise call the function again
        (ash integer -1)                   ;; Right-shift the integer by one
        (cons (logand integer 1) carry)))) ;; Cons this bit to the carry list
@@ -16,28 +18,50 @@
 (defun int-to-bits (i)
   (recurse-collect-bits (abs i) '())) ;; Get bits from the absolute value of input
 
-;; Recursively pad a list to a desired length with a desired pad item
-(defun pad-list (desired-length pad collector)
-  (if (<= desired-length (length collector))             ;; If the desired length is >= actual length
-    collector                                            ;; Return the collector
-    (pad-list desired-length pad (cons pad collector)))) ;; Else pad and recurse
+;; Fills a list with an item using the difference between target-length and current-length
+(defun create-pad (target-length current-length pad)
+  (make-list (- target-length current-length) :initial-element pad))
+
+;; Pad a list to a desired length with a desired pad item on the left
+(defun pad-list-left (desired-length pad input)
+  (let ((input-length (length input)))
+    (if (<= desired-length input-length)    ;; If the desired length is >= actual length
+      input                                 ;; Return the input
+      (concatenate                          ;; Otherwise, create a pad list and concatenate on the left
+       'list
+       (create-pad desired-length input-length pad)
+       input))))
+
+;; Like pad-list-left, but pads on the right instead
+(defun pad-list-right (desired-length pad input)
+  (let ((input-length (length input)))
+    (if (<= desired-length input-length)
+      input
+      (concatenate
+       'list
+       input
+       (create-pad desired-length input-length pad)))))
+
+;; Get the next number after current divisible by n
+(defun next-divisible (current n)
+  (+ current (- n (rem current n))))
 
 ;; Encode an integer of arbitrary length (up to 255 bits)
 (defun encode-arbitrary-int (integer)
   (let* ((bit-length-as-bits (int-to-bits (integer-length integer))) ;; Get the bit length
-         (padded-bit-length  (pad-list 8 0 bit-length-as-bits))      ;; Pad it to 8 to standardize size
+         (padded-bit-length  (pad-list-left 8 0 bit-length-as-bits)) ;; Pad it to 8 to standardize size
          (sign               (if (< 0 integer) 1 0))                 ;; Determine sign-bit
          (integer-bits       (int-to-bits integer)))                 ;; Get the bits of the number
     (concatenate 'list padded-bit-length (list sign) integer-bits))) ;; Concatenate length as bits and real bits
 
 ;; Recursively turn a list of bits into an integer
 (defun recurse-decode-int (len bits collector)
-  (if (equalp len 0)                             ;; If length is zero
-    collector                                    ;; Return collector, else
-    (recurse-decode-int                          ;; Recurse
-     (- len 1)                                   ;; Subtracting len by 1
-     (rest bits)                                 ;; With only the tail of the bit list
-     (logior (ash collector 1) (first bits)))))  ;; And the result of (collector << 1) | (first bits)
+  (if (zerop len)                               ;; If length is zero
+    collector                                   ;; Return collector, else
+    (recurse-decode-int                         ;; Recurse
+     (- len 1)                                  ;; Subtracting len by 1
+     (rest bits)                                ;; With only the tail of the bit list
+     (logior (ash collector 1) (first bits))))) ;; And the result of (collector << 1) | (first bits)
 
 ;; One-argument wrapper for recurse-decode-int
 (defun decode-int (bits)
@@ -46,7 +70,7 @@
 ;; Decode an encoded integer of arbitrary length
 (defun decode-arbitrary-int (bits)
   (let ((int-length (decode-int (subseq bits 0 8))))                 ;; Extract length of encoded int
-    (if (equalp int-length 0)                                        ;; If the length of the encoded int is zero
+    (if (zerop int-length)                                           ;; If the length of the encoded int is zero
       0                                                              ;; Return early
       (let ((sign (if (equalp 0 (nth 9 bits)) -1 1))                 ;; Otherwise, extract the sign bit
             (integer (decode-int (subseq bits 9 (+ 9 int-length))))) ;; Extract the integer bits and decode
@@ -122,3 +146,30 @@
          (packet-schema        (getf registry packet-id))
          (encoded-packet       (mapcan (field-encoder packet-schema) packet-data)))
    (concatenate 'list (encode-arbitrary-int packet-discriminator) encoded-packet)))
+
+(defun recurse-partition (list len coll)
+  (if list
+    (recurse-partition
+     (nthcdr len list)
+     len
+     (cons (subseq list 0 len) coll))
+    (reverse coll)))
+
+(defun partition (list len)
+  (recurse-partition list len '()))
+
+
+(defun bits->bytes (bits)
+  (let ((byte-bit-lists (partition bits 8)))
+    (map 'list #'decode-int byte-bit-lists)))
+  
+
+(defun pad-bits-and-make-bytes (bits)
+  (let* ((pad-length  (next-divisible (length bits) 8))
+         (padded-bits (pad-list-right pad-length 0 bits))
+         (partitioned (partition padded-bits 8)))
+    (mapcan #'bits->bytes partitioned)))
+
+(defun bytes->bits (bytes)
+  (let ((bits-list (map 'list #'int-to-bits bytes)))
+    (mapcan (lambda (bits) (pad-list-left 8 0 bits)) bits-list)))
